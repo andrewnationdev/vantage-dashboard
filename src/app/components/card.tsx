74634e3e007597867a3aa421c50adac36dd386df
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2026-01-02 14:59:35 +0000

    primeira prévia - estilização

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -4,15 +4,26 @@ import { IStatsCardComponent } from "../types/types";
 import { BanknoteArrowDown, DollarSign, ArrowUp } from "lucide-react"
 
 export default function StatsCardComponent(props: IStatsCardComponent) {
-    return <div className="bg-white p-6 rounded-xl border border-slate-200 shadow-sm flex flex-col gap-2 hover:shadow-md transition-shadow">
+    function getCardBackground():string {
+        if(props.title.includes("saldo") || props.title.includes("Saldo")){
+            return "bg-teal-900"
+        } else if(props.title.includes("entradas") || props.title.includes("Entradas")){
+            return "bg-stone-900"
+        } else if(props.title.includes("saídas") || props.title.includes("Saídas")){
+            return "bg-rose-900"
+        }
+        else return "bg-slate-700"
+    }
+
+    return <div className={`${getCardBackground()} p-6 text-slate-100 rounded-xl border border-slate-500 shadow-sm flex flex-col gap-2 hover:shadow-md transition-shadow`}>
         <div className="flex gap-4 justify-between">
             <span className="font-bold">{props.title}</span>
             {props.title.includes("saldo") || props.title.includes("Saldo")
-                && <DollarSign color="green"/>}
+                && <DollarSign color="#2dd4bf"/>}
             {props.title.includes("entradas") || props.title.includes("Entradas")
-                && <ArrowUp color="grey"/>}
+                && <ArrowUp color="#93c5fd"/>}
             {props.title.includes("saídas") || props.title.includes("Saídas")
-                && <BanknoteArrowDown color="red"/>}
+                && <BanknoteArrowDown color="#e11d48"/>}
         </div>
         <div className="flex gap-4 flex-col">
             <span className="font-bold text-2xl">{props.value}</span>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,20 @@ import StatsCardComponent from "./components/card";
 import { useEffect, useState } from "react";
 import { ISummaryCard } from "./types/types";
 import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts';
+import { Menu } from "lucide-react"
 
 type TData = ISummaryCard[] | []
 
 export default function DashboardPage() {
   const [data, setData] = useState<TData>([]);
+  const [year, setYear] = useState<number | string>("");
 
   useEffect(() => {
     const fetchData = () => {
       const res = SUMMARY_CARDS
+      const currentYear = new Date().getFullYear();
 
+      setYear(currentYear);
       setData(res as TData);
     }
 
@@ -21,11 +25,14 @@ export default function DashboardPage() {
   }, [])
 
   return (
-    <main className="min-h-screen bg-slate-50 p-4 md:p-8">
+    <main className="min-h-screen bg-gray-800 p-4 md:p-8">
       <div className="max-w-7xl mx-auto space-y-8">
 
-        <header className="max-w">
-          <h1 className="text-3xl font-bold text-slate-900">Vantage Dashboard</h1>
+        <header className="max-w flex justify-between">
+          <h1 className="text-3xl font-bold text-slate-100">Vantage Dashboard</h1>
+          <button className="">
+            <Menu color="#22d3ee"/>
+          </button>
         </header>
 
         <section className="grid grid-cols-1 md:grid-cols-3 gap-6">
@@ -37,18 +44,24 @@ export default function DashboardPage() {
               description={item.description} />)}
         </section>
 
-        <section className="bg-white p-6 rounded-xl shadow-sm border border-slate-200">
-          <h2 className="text-xl font-semibold mb-6">Fluxo de Caixa</h2>
-          <div className="h-[300px] w-full flex items-center justify-center text-slate-400">
-            <LineChart style={{ width: '100%', aspectRatio: 1.618, maxWidth: 800, margin: 'auto' }} responsive data={FINANCIAL_DATA}>
-              <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+        <section className="bg-slate-700 p-6 rounded-xl shadow-sm border border-slate-500">
+          <h2 className="text-xl text-slate-100 font-semibold mb-6">Fluxo de Caixa</h2>
+          <div className="w-full flex items-center justify-center text-slate-100">
+            <LineChart style={{ width: '100%', color: 'white', aspectRatio: 1.618, maxWidth: 800, margin: 'auto' }} responsive data={FINANCIAL_DATA}>
+              <CartesianGrid stroke="#eee" strokeDasharray="9 9" />
               <XAxis dataKey="month" />
               <YAxis width="auto" />
-              <Line type="monotone" dataKey="income" stroke="#8884d8" />
-              <Line type="monotone" dataKey="expense" stroke="#82ca9d" />
+              <Line type="monotone" dataKey="income" stroke="#00a9c7ff" />
+              <Line type="monotone" dataKey="expense" stroke="#bb29f5ff" />
             </LineChart>
           </div>
         </section>
+
+        <footer className="flex justify-center">
+          <p className="text-slate-100">© {year} - Todos os Direitos Reservados. Desenvolvido por 
+            <a href="https://andrewnationdev.vercel.app/" className="text-sky-300 hover:text-sky-100 ml-2">AndrewNation</a>
+          </p>
+        </footer>
 
       </div>
     </main>
