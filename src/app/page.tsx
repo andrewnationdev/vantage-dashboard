c69841ad20d6d328a0ce4b2de2685a4270ff48d1
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2026-01-04 14:52:32 +0000

    improve animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ export default function DashboardPage() {
   const [year, setYear] = useState<number | string>("");
   const [loading, setLoading] = useState<boolean>(true);
   const [statusText, setStatusText] = useState<string>("");
+  const [failedFetching, setFailedFetching] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,10 +22,12 @@ export default function DashboardPage() {
       const res = await fetch(`/api/finance`);
       const json = await res.json()
 
-      if(res.status == 200){
+      if (res.status == 200) {
         setLoading(false);
+        setFailedFetching(false);
       } else {
         setStatusText("Erro ao obter os dados");
+        setFailedFetching(true);
       }
 
       setData(json);
@@ -37,14 +40,15 @@ export default function DashboardPage() {
 
   return (
     <main className="min-h-screen bg-gray-800 p-4 md:p-8">
-      <div className="max-w-7xl mx-auto space-y-8">
-        <AppHeader/>
-        {!loading && <FinancialCardsRowComponent data={data?.summary}/>}
-        {!loading && <ChartComponent data={data?.chartData}/>}
+      <div className={`transition-opacity duration-1200 ${!loading ? 'opacity-100' : 'opacity-0'} max-w-7xl mx-auto space-y-8`}>
+        <AppHeader />
+        {!loading && !failedFetching && <FinancialCardsRowComponent data={data?.summary} />}
+        {!loading && !failedFetching && <ChartComponent data={data?.chartData} />}
         {loading && <section className="bg-slate-700 p-6 rounded-xl shadow-sm border border-slate-500">
-        <h2 className="text-xl text-center text-slate-100 font-semibold mb-6">{statusText}</h2>
+          <h2 className="text-xl text-center text-slate-100 font-semibold mb-6">
+            {statusText}</h2>
         </section>}
-        <AppFooter year={year}/>
+        <AppFooter year={year} />
       </div>
     </main>
   );
