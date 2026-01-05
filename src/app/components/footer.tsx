6fed3fb93b84360dbbd8ddb0a1b895f1a88feb89
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2026-01-05 14:15:32 +0000

    hover no link

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,7 +3,7 @@ import { IFooter } from "../types/types";
 export default function AppFooter(props: IFooter) {
     return <footer className="flex justify-center text-center">
         <p className="text-slate-100">© {props.year} - Todos os Direitos Reservados. Desenvolvido por
-            <a href="https://andrewnationdev.vercel.app/" className="text-sky-300 hover:text-sky-100 ml-2">AndrewNation</a>
+            <a href="https://andrewnationdev.vercel.app/" className="text-sky-300 hover:text-sky-100 hover:underline ml-2">AndrewNation</a>
         </p>
     </footer>
 }
